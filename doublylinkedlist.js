8e0cf6c09c85530e4cf6741ff4ca5a7afb039628
wenlinf <wenlin.fang9664@gmail.com>
2020-11-25 20:08:43 +0800

    add push method for doublylinkedlist

---
+++ doublylinkedlist.js
@@ -0,0 +1,28 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+}
