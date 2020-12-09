aabe3417ad01c0da01c738ad3305e50960a643e2
wenlinf <wenlin.fang9664@gmail.com>
2020-12-09 23:23:57 +0800

    added methods in priority queue

--- Priority_queue.js
+++ Priority_queue.js
@@ -12,6 +12,33 @@ class PriorityQueue{
             index = Math.floor((index-1)/2)
         }
     }
+    dequeue(){
+        var min = this.values[0];
+        var end = this.values.pop();
+        if(this.values.length>0){
+            var endPriority = end.priority;
+            var index = 0;
+            this.values[index] = end;
+            while(true){
+                var leftChildIndex = index*2+1;
+                var rightChildIndex = index*2+2;
+                var swap = null;
+                if(leftChildIndex<this.values.length){
+                    var leftChild = this.values[leftChildIndex];
+                    if(endPriority>leftChild.priority) swap = leftChildIndex;
+                }
+                if(rightChildIndex<this.values.length){
+                    var rightChild = this.values[rightChildIndex];
+                    if((swap!=null && rightChild.priority<leftChild.priority)||(swap === null && rightChild.priority<end.priority)) swap = rightChildIndex;
+                }
+                if(swap === null) break;
+                this.values[index] = this.values[swap];
+                this.values[swap] = end;
+                index = swap;
+            }
+        }
+        return min;
+    }
 }
 
 class Node{
