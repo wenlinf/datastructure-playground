34c820527afa0e3f72f5180d3dba419c1e008382
wenlinf <wenlin.fang9664@gmail.com>
2020-12-12 08:50:32 +0800

    add methods

--- graph.js
+++ graph.js
@@ -7,4 +7,31 @@ class Graph{
             this.adjacencyList[vertex] = [];
         }
     }
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+    removeEdge(vertex1,vertex2){
+        var index= this.adjacencyList[vertex1].indexOf(vertex2);
+        this.adjacencyList[vertex1].splice(index,1);
+        index = this.adjacencyList[vertex2].indexOf(vertex1);
+        this.adjacencyList[vertex2].splice(index,1);
+    }
+    removeVertex(vertex){
+        var edges = this.adjacencyList[vertex];
+        for(var i =0;i<edges.length;i++){
+            this.removeEdge(vertex,edges[i]);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+    
 }
+
+let g = new Graph();
+g.addVertex("Dallas");
+g.addVertex("Tokyo");
+g.addVertex("Aspen");
+g.addEdge("Dallas", "Tokyo");
+g.addEdge("Dallas", "Aspen");
+
